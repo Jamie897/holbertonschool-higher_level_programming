48d3dd2df25503f441b7c0226bd65d9eb91126a7
Jamie897 <114971783+Jamie897@users.noreply.github.com>
2023-05-29 14:13:32 -0500

    6-base_geometry.py

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Public instance method: def area(self): that raises an Exception with the message area() is not implemented
+
+You are not allowed to import any module 
+"""
+
+
+classBaseGeometry:
+    """Only raises exceptions"""
+    def area(self):
+        """areamethod"""
+        raise Exception("area() is not implemented")
