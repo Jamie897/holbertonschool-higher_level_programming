de5fe3f26ced2eed6dacb951b1001421b7504918
Jamie897 <114971783+Jamie897@users.noreply.github.com>
2023-06-03 13:54:38 -0500

    base.py

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -1,46 +1,66 @@
-#!/usr/bin/python3
-"""
-class Base:
-
-private class attribute __nb_objects = 0
-class constructor: def __init__(self, id=None)::
-if id is not None, assign the public instance attribute
-    id with this argument value - you can assume id is an
-    integer and you dont need to test the type of it
-otherwise, increment __nb_objects and assign the new
-    value to the public instance attribute id
-"""
+       #!/usr/bin/python3
+''' models/base.py
+'''
 import json
 
 
-class Base():
-    """Base class for all shapes"""
+class Base:
+    ''' first class Base
+    '''
 
     __nb_objects = 0
 
     def __init__(self, id=None):
-        """Init method"""
+        ''' initialize Base  __init__
+        '''
         if id is not None:
             self.id = id
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
 
-    @staticmethod
-    def to_json_string(list_dictionaries):
-        """Func to make into json file"""
-        if list_dictionaries is None or len(list_dictionaries) == 0:
-            return "[]"
-        else:
-            return json.dumps(list_dictionaries)
-
     @classmethod
     def save_to_file(cls, list_objs):
-        """Func to write json str to file"""
-        filename = cls.__name__ + ".json"
-        json_list = []
+        '''method: save_to_file
+        '''
+        list_dicts = []
         if list_objs is not None:
-            json_list = [obj.to_dictionary() for obj in list_objs]
+            for obj in list_objs:
+                tmp_dict = cls.to_dictionary(obj)
+                list_dicts.append(tmp_dict)
+            json_L_of_D = cls.to_json_string(list_dicts)
+        else:
+            json_L_of_D = "[]"
+
+        filename = cls.__name__ + ".json"
+
         with open(filename, "w") as file:
-            file.write(cls.to_json_string(json_list))
-        
+            num_char = file.write(json_L_of_D)
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        ''' method json string
+        '''
+        ret_list = []
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return "[]"
+
+        if type(list_dictionaries) != list:
+            ret_list.append(list_dictionaries)
+        else:
+            ret_list = list_dictionaries
+        return json.dumps(ret_list)
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        ''' method json string
+        '''
+        ret_list = []
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return "[]"
+
+        if type(list_dictionaries) != list:
+            ret_list.append(list_dictionaries)
+        else:
+            ret_list = list_dictionaries
+        return json.dumps(ret_list) 
