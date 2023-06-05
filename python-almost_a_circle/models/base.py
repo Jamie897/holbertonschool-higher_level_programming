f76a51e8377bd316b4c020503e804daed6ffc7b1
Jamie897 <114971783+Jamie897@users.noreply.github.com>
2023-06-04 20:51:15 -0500

    base.py

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -21,7 +21,8 @@ class Base():
 
     __nb_objects = 0
 
-    def __init__(self, id=None):
+    def __init__(self):
+        self.id = None
         """Initializes the default attributes of the Base object.
 
         Args:
